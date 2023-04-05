4990857dda4c8334a2de6e1829d32c6bc91c2ed1
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-04-05 14:47:55 +0300

    init

---
+++ main.py
@@ -0,0 +1,32 @@
+print("Merhaba Etiya") 
+print("Hoş geldiniz")
+
+# yorum
+
+# değişkenler -start
+# metinsel, numerik, obje
+
+# string
+text = "Merhaba"
+
+print(text)
+print(text)
+print(text)
+text = "Selam"
+print(text)
+print(text)
+
+studentCount = 45 # int, integer => tam sayı
+print(studentCount + 5)
+
+averagePoint = 25.5 
+print(averagePoint + 5)
+
+isVerified = True # bool, boolean => True veya False
+print(isVerified)
+
+print(type(text))
+print(type(studentCount))
+print(type(averagePoint))
+print(type(isVerified))
+# değişkenler -end
