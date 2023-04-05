7bb56aa4ac6b0e1bb1d40526a744b055ffc05c1f
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-04-05 15:17:10 +0300

    operators

--- main.py
+++ main.py
@@ -30,3 +30,36 @@ print(type(studentCount))
 print(type(averagePoint))
 print(type(isVerified))
 # değişkenler -end
+
+# operatörler -start
+number = 10
+# matematiksel operatörler 
+print(10 + 10)
+print(number + number)
+
+print(number - 5)
+
+print(number / 2)
+
+print(number * 5)
+
+# mod operatörü => sol tarafdaki değerin sağ taraftaki değere bölümünden kalan sonuç
+print(number % 3)
+
+
+print(number + ((10 - number) * (5 / 10)))
+
+# mantıksal operatörler - karşılaştırma operatörleri
+print(10 == 10) #true
+print(11 == 10) #false
+
+print(10 != 10) #false
+print(11 != 10) #true
+
+print(number > 10) #false
+print(number >= 10) #true
+
+print(number < 10) #false
+print(number <= 10) #true
+
+# operatörler -end
