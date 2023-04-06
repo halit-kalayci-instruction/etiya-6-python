d623983b50551998bea0faa185f923e9d25ad36c
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-04-06 15:00:03 +0300

    Personal Work - Result

--- main.py
+++ main.py
@@ -143,9 +143,22 @@ ortalama = (vize * 0.4) + (final * 0.6)
 # eğer vize finalin 2 katı ise kullanıcı kaldı
 # bunun dışındaki tüm durumlarda kullanıcı geçti yazdırmak istiyoruz.
 
-
+#condition-logic
+# or and
+# (true or false) => true => sol ve sağındaki koşullardan en az birisinin true olmasını istiyor 
+# (true and false) => false => sol ve sağındaki koşulların ikisinin de kesinlikle true olmasını ister
+if final < 40 or ortalama < 50 or vize == final *2:
+    print("Kaldı")
+# elif ortalama < 50:
+#     print("Kaldı")
+# elif vize == final * 2:
+#     print("Kaldı")
+else:
+    print("Geçti")
 
 # karar yapıları -end
+
+
 
 
 
