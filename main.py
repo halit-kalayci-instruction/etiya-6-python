7a346a098d02acd8c5fbed06a11361e19b828c0a
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-04-06 14:26:47 +0300

    Personal Workshop 1

--- main.py
+++ main.py
@@ -132,4 +132,20 @@ if studentCount > 20:
     print("Öğrenciler ders için hazır")
 
 print("İf bloğundan bağımsız kısım")
+
+
+vize = int(input("Vize notunuzu giriniz: "))
+final = int(input("Final notunuzu giriniz: "))
+ortalama = (vize * 0.4) + (final * 0.6) 
+
+# eğer final 40'dan küçükse kullanıcı kaldı
+# eğer ortalama 50'den küçükse kullanıcı kaldı
+# eğer vize finalin 2 katı ise kullanıcı kaldı
+# bunun dışındaki tüm durumlarda kullanıcı geçti yazdırmak istiyoruz.
+
+
+
 # karar yapıları -end
+
+
+
