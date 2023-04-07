18053d071557b95b0e455264b9f44adf3944aeb4
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-04-07 13:47:11 +0300

    workshop-3 ister1-2.cözüm

--- workshop-3.py
+++ workshop-3.py
@@ -1,21 +1,22 @@
 #kulanıcıdan 10 adet sayı alalım ✅
-#ve bu sayılar arasından en büyük 2. olanı kullanıcıya gösterelim
+#ve bu sayılar arasından en büyük 2. olanı kullanıcıya gösterelim ✅
 
 #döngüler-loops
 #for loop
 # i=0 , i = i + 1
 # 0,1,2,3,4,5,6,7,8,9  < 10
 
-biggestValue = 0 # 14,25
+sayilar = []
 # 0 dan başlat, 10'dan küçük olana kadar döngüyü çalıştır, 
 # döngü her çalıştığında i değerini 1 artır
-for i in range(10):
-    sayi = int(input(f"{i+1}. sayıyı giriniz: ")) #25
-    if sayi > biggestValue:
-        biggestValue = sayi
+for i in range(3):
+    sayilar.append(int(input(f"{i+1}. sayıyı giriniz: "))) #25
+sayilar.sort(reverse=True)
+enBuyukN = int(input("Sayılar arasından en büyük kaçıncı elemanı istiyorsun? "))
+print(sayilar[enBuyukN - 1])
 
-    
-print(f"Girdiğiniz sayılar arasından en büyük olanı: {biggestValue}")
+
+## end
 
 
     
